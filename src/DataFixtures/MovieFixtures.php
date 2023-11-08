5ab836eb2546ef2bece64dd407a56e16f004374e
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-11-08 20:34:19 +0100

    Creating a front-end where we will work

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -14,7 +14,7 @@ class MovieFixtures extends Fixture
         $movie->setTitle("The Dark Knight");
         $movie->setReleaseYear(2008);
         $movie->setDescription("This is a description of The Dark Night");
-        $movie->setImagePath("https://cdn.pixabay.com/photo/2023/03/14/22/20/relationship-7853278_1280.jpg");
+        $movie->setImagePath("https://cdn.pixabay.com/photo/2021/06/18/11/22/batman-6345897_960_720.jpg");
         // Add data to pivot table
         $movie->addActor($this->getReference("actor_1"));
         $movie->addActor($this->getReference("actor_2"));
@@ -25,7 +25,7 @@ class MovieFixtures extends Fixture
         $movie2->setTitle("Avengers: Endgame");
         $movie2->setReleaseYear(2019);
         $movie2->setDescription("This is a description of Avengers: Endgame");
-        $movie2->setImagePath("https://cdn.pixabay.com/photo/2023/06/23/17/12/thor-8083865_1280.jpg");
+        $movie2->setImagePath("https://cdn.pixabay.com/photo/2020/07/02/19/36/marvel-5364165_960_720.jpg");
         $movie2->addActor($this->getReference("actor_3"));
         $movie2->addActor($this->getReference("actor_4"));
         $manager->persist($movie2);
